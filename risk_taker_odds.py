01daf148385b18a3284672cf966513416af7047c
Dmitriy Severov <Dmitriy.Severov@bia-tech.ru>
2023-02-21 00:06:24 +0500

    Add new battles

--- risk_taker_odds.py
+++ risk_taker_odds.py
@@ -38,7 +38,6 @@ def run_simulation_3_2():
         d2 = roll_dice()
         dlist = [d1, d2]
         dlist.sort(reverse=True)
-        #print(alist, dlist, sep="\n", end="\n\n")
         # Results
         if alist[0] > dlist[0]: # in case of tie, defender wins
             losses["DEF"] += 1
@@ -49,6 +48,7 @@ def run_simulation_3_2():
         else:
             losses["ATK"] += 1
     return losses
+
 
 def run_simulation_2_2():
     losses = {"ATK":0, "DEF":0}
@@ -65,7 +65,6 @@ def run_simulation_2_2():
         d2 = roll_dice()
         dlist = [d1, d2]
         dlist.sort(reverse=True)
-        #print(alist, dlist, sep="\n", end="\n\n")
         # Results
         if alist[0] > dlist[0]: # in case of tie, defender wins
             losses["DEF"] += 1
@@ -78,6 +77,48 @@ def run_simulation_2_2():
     return losses
 
 
+def run_simulation_3_1():
+    losses = {"ATK":0, "DEF":0}
+    for _ in range(battles_amount):
+        # Battle action
+        # 3 atackers vs 1 defender
+        # ATK
+        a1 = roll_dice()
+        a2 = roll_dice()
+        a3 = roll_dice()
+        alist = [a1, a2, a3]
+        alist.sort(reverse=True)
+        # DEF
+        d1 = roll_dice()
+        # Results
+        if alist[0] > d1: # in case of tie, defender wins
+            losses["DEF"] += 1
+        else:
+            losses["ATK"] += 1
+    return losses
+
+
+def run_simulation_2_1():
+    losses = {"ATK":0, "DEF":0}
+    for _ in range(battles_amount):
+        # Battle action
+        # 2 atackers vs 1 defender
+        # ATK
+        a1 = roll_dice()
+        a2 = roll_dice()
+        alist = [a1, a2]
+        alist.sort(reverse=True)
+        # DEF
+        d1 = roll_dice()
+        # Results
+        if alist[0] > d1: # in case of tie, defender wins
+            losses["DEF"] += 1
+        else:
+            losses["ATK"] += 1
+    return losses
+
+
+
 #===================== MAIN =====================
 
 def main():
@@ -85,15 +126,31 @@ def main():
     print("Battles: ", battles_amount)
     losses_3_2 = run_simulation_3_2()
     losses_2_2 = run_simulation_2_2()
+    losses_3_1 = run_simulation_3_1()
+    losses_2_1 = run_simulation_2_1()
     print("=" * 16 + " Done. " + "=" * 16)
+
     print("Losses 3 vs 2:", losses_3_2)
     math_val_atk = losses_3_2["ATK"] / battles_amount
     math_val_def = losses_3_2["DEF"] / battles_amount
     print("ATK math val:", math_val_atk)
     print("DEF math val:", math_val_def)
+
     print("Losses 2 vs 2:", losses_2_2)
     math_val_atk = losses_2_2["ATK"] / battles_amount
     math_val_def = losses_2_2["DEF"] / battles_amount
+    print("ATK math val:", math_val_atk)
+    print("DEF math val:", math_val_def)
+
+    print("Losses 3 vs 1:", losses_3_1)
+    math_val_atk = losses_3_1["ATK"] / battles_amount
+    math_val_def = losses_3_1["DEF"] / battles_amount
+    print("ATK math val:", math_val_atk)
+    print("DEF math val:", math_val_def)
+
+    print("Losses 2 vs 1:", losses_2_1)
+    math_val_atk = losses_2_1["ATK"] / battles_amount
+    math_val_def = losses_2_1["DEF"] / battles_amount
     print("ATK math val:", math_val_atk)
     print("DEF math val:", math_val_def)
 
